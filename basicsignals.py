597cced94530889af111caa1db00f887aa31a358
Pranav <pranavnat24@gmail.com>
2019-06-13 22:29:35 +0530

    Basic Signals - Sine

--- basicsignals.py
+++ basicsignals.py
@@ -5,6 +5,7 @@ import numpy as np
 import pylab
 import time
 import pyqtgraph
+from scipy import signal
 
 class ExampleApp(QtGui.QMainWindow, bsui.Ui_MainWindow):
     def __init__(self, parent=None):
@@ -15,23 +16,29 @@ class ExampleApp(QtGui.QMainWindow, bsui.Ui_MainWindow):
         self.plotter.plotItem.showGrid(True, True, 0.7)
         self.npoints.setMinimum(1)
         self.npoints.setMaximum(10)
+        self.points = 1000 #number of data points
+        self.X = np.arange(self.points)
+
 
     def update(self):
-        t1=time.clock()
-        points, X, Y = self.sine()
-        C=pyqtgraph.hsvColor(time.time()/5%1,alpha=.8)
-        pen=pyqtgraph.mkPen(color=C,width=10)
+        X = self.X
+        t1 = time.clock()
+        Y = self.sine()
+        C = pyqtgraph.hsvColor(time.time() / 5%1, alpha=.5)
+        pen = pyqtgraph.mkPen(color=C, width=5)
         self.plotter.plot(X,Y,pen=pen,clear=True)
-        print("update took %.02f ms"%((time.clock()-t1)*1000))
+        print("Update took %.02f ms"%((time.clock() - t1) * 1000))
         if self.updatcheckbox.isChecked():
             QtCore.QTimer.singleShot(1, self.update) # QUICKLY repeat
 
     def sine(self):
-        points=1000 #number of data points
-        X=np.arange(points)
-        c = self.npoints.value()
-        Y=np.sin(np.arange(points)/points*c*np.pi+time.time())
-        return points, X, Y
+        self.c = self.npoints.value()
+        Y = np.sin(np.arange(self.points) / self.points * self.c * np.pi + time.time())
+        return Y
+
+    def exponential(self):
+        self.c = self.npoints.value()
+        Y = np.exp()
 
 
 
